4246b4a18d0204e31d501c5610c3f712e504502a
Nikasvirid <sviridka-vp@mail.ru>
2022-05-23 10:44:31 +0300

    feat:add code

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,42 +1,37 @@
+//const fs = require('fs');
+//const path = require('path');
+//const {stat} = require('fs');
+//fs.readdir('./03-files-in-folder/secret-folder',(err,files) => {
+// if(err) throw err;
+//console.log ('Перечень файлов:'+ files);
+//fs.stat('./03-files-in-folder/secret-folder/',(err, stats) =>{
+//if(err){
+//console.error(err);
+//return;
+//}
+//console.log(stats);
+//});
+//});
+
+
+const { readdir } = require('fs/promises');
 const fs = require('fs');
 const path = require('path');
-const {stat} = require('fs');
-fs.readdir('./03-files-in-folder/secret-folder',(err,files) => {
-  if(err) throw err;
-  console.log ('Перечень файлов:'+ files);
-  fs.stat('./03-files-in-folder/secret-folder/',(err, stats) =>{
-    if(err){
-      console.error(err);
-      return;
+
+async function getFiles() {
+  try {
+    const files = await readdir(path.join(__dirname, 'secret-folder'), 'utf8', true);
+    for (const file of files) {
+      const extname = path.parse(file);
+      fs.stat(path.join(__dirname, 'secret-folder', file), (err, stats) => {
+        if (err) throw err;
+        if (stats.isFile())
+          console.log(extname.name + ' - ' + extname.ext.substring(1, extname.ext.lenght) + ' - ' + stats.size);
+      });
     }
-    console.log(stats);
-    
-  });
-});
-try{
-  const files = fs.readdir(path.join(__dirname, 'secret-folder'), {withFileTypes: true});
-  files.then(arrayOfFiles=>{
-    arrayOfFiles.forEach(async dirent=>{
-      if(dirent.isFile()){
-        const {name, ext} = path.parse(`${dirent.name}`);
-        stat(path.join(__dirname,'secret-folder',`${dirent.name}`), (err, stats) => {
-          console.log(`${name} - ${ext} - ${stats.size}`);
-        });               
-      }
-    });
-  });
-}catch(err){
-  console.error(err);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-
-
-
-
-
-
-
-
-
-   
-
+getFiles();
