e44b39c414924e4727fe31b2896489eb2697ab86
Nikasvirid <sviridka-vp@mail.ru>
2022-05-23 18:44:29 +0300

    feat :fixed schanges

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,60 +5,40 @@
 //console.log('Все папки успешно созданы');
 //});
 
-const { readdir } = require('fs/promises');
 const fs = require('fs');
 const path = require('path');
 
-async function copyFiles(pathSrc, pathDest, dir, prefix) {
-  makeDir(pathDest, dir, prefix);
-  try {
-    const files = await readdir(path.join(pathSrc, dir), 'utf8', true);
-    for (const file of files) {
-      fs.stat(path.join(pathSrc, dir, file), (err, stats) => {
-        if (err) throw err;
-        if (stats.isFile()) {
-          const newDir = dir + prefix;
-          copyFile(path.join(pathSrc, dir), path.join(pathDest, newDir), file);
-        } else {
-          const newDir = dir + prefix;
-          copyFiles(path.join(pathSrc, dir), path.join(pathDest, newDir), file, '');
-        }
-      });
+const srcDir = path.join(__dirname, 'files');
+const destDir = path.join(__dirname, 'files-copy');
 
-    }
-  } catch (err) {
-    console.log('Что-то пошло не так с файлами');
+fs.access(destDir, (err) => {
+  if (err) {
+    copyAllFiles(srcDir, destDir);
   }
+  else fs.rmdir(destDir, { force: true, recursive: true }, function(err) {
+    if (err) throw err;
+    copyAllFiles(srcDir, destDir);
+  });
+});
+
+function copyFile(src, dest, file) {
+  fs.copyFile(path.join(src, file.name), path.join(dest, file.name), err => {
+    if (err) throw err;
+  });
+  console.log(`${file.name} файл успешно скопирован`);
 }
 
-async function makeDir(pathFolder, dir, prefix) {
-  try {
-    const newDir = dir + prefix;
-    await fs.promises.mkdir(path.join(pathFolder, newDir), true);
-  } catch (err) {
-    //console.log('Ошибка ', err)
-  }
-
-}
-async function copyFile(pathSrc, pathDest, file) {
-  try {
-    await fs.promises.copyFile(path.join(pathSrc, file), path.join(pathDest, file));
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-function runCopy() {
-  fs.access(path.join(__dirname, 'files-copy'), function (err) {
-    if (err && err.code === 'ENOENT') {
-      copyFiles(__dirname, __dirname, 'files', '-copy');
-    } else {
-      fs.rm(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
-        if (err) throw err;
-        copyFiles(__dirname, __dirname, 'files', '-copy');
-      });
-    }
+function copyAllFiles (src, dest) {
+  fs.mkdir(dest, (err) => {
+    if (err) throw err;
+  });
+  fs.readdir(src, {withFileTypes: true}, (err, files) => {
+    files.forEach(file => {
+      if(file.isFile()) {
+        copyFile(src, dest, file);
+      } else {
+        copyAllFiles(path.join(src, file.name), path.join(dest, file.name));
+      }
+    });
   });
 }
-
-runCopy();
